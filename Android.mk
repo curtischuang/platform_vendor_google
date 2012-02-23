f27f740251d542f547c99480f000bed6627a2bbb
curtis chuang <curtis@deka-builder.(none)>
2012-02-23 14:44:20 +0800

    test

--- Android.mk
+++ Android.mk
@@ -1,2 +1,3 @@
 # We need a way to prevent the stuff Google Apps replaces from being included in the build. This is a hacky way to do that.
 PACKAGES.Phone.OVERRIDES := Calendar CalendarProvider Email Exchange Gallery2 PicoTts
+#curtis
